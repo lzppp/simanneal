8af5f566e29fe938ed93beb991b8a203f9abcd54
lzppp <lzppp1114@gmail.com>
2017-01-13 21:42:44 +0800

    flow

--- examples/salesman.py
+++ examples/salesman.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import math
 import random
 from simanneal import Annealer
+import networkx as nx
 
 
 def distance(a, b):
@@ -25,7 +26,7 @@ class TravellingSalesmanProblem(Annealer):
 
     def move(self):
         """Swaps two cities in the route."""
-        a = random.randint(0, len(self.state) - 1)
+        a = random.randint(0, len(self.state) - 1)#
         b = random.randint(0, len(self.state) - 1)
         self.state[a], self.state[b] = self.state[b], self.state[a]
 
@@ -36,56 +37,112 @@ class TravellingSalesmanProblem(Annealer):
             e += self.distance_matrix[self.state[i-1]][self.state[i]]
         return e
 
+class recalculatebySA(Annealer):
+    def distance(self , tpath ,graph):
+        '''
+            cost+=graph[][]['cost']
+        '''
+        if len(tpath) == 2:
+            return self.graph[tpath[0]][tpath[1]]['cost']
+        if len(tpath) == 1:
+            return 0
+        cost = 0
+        for i in (0,len(tpath)-2):
+            cost += self.graph[tpath[i]][tpath[i+1]]['cost']
+        return cost
+    def __init__(self , state , graph):
+        self.graph = graph
+        self.path = {}
+        self.flow = {}
+        super(recalculatebySA, self).__init__(state) 
+    def move(self):
+        
+        for flowkey in self.flow:
+            self.state[flowkey] = self.path[flowkey][random.randint(0, len(self.path[flowkey]) - 1)]
+
+
+    def energy(self):
+        '''
+            all the cost
+        '''
+        e = 0
+        for flowkey in self.flow:
+            e += self.distance(self.state[flowkey] , self.graph)
+
+        return float(e)
+
 
 
 if __name__ == '__main__':
+    '''
+        test function
+    '''
+    # init a graph
+    g = nx.Graph()
+    g.add_edge(1,2,weight=1)
+    g.add_edge(1,3,weight=1)
+    g.add_edge(2,4,weight=1)
+    g.add_edge(3,4,weight=1)
+    g[1][2]['cost'] = 2
+    g[1][3]['cost'] = 2
+    g[2][4]['cost'] = 2
+    g[3][4]['cost'] = 2
+    g.add_edge(2,3,weight=1)
+    g[2][3]['cost'] = 3
 
-    # latitude and longitude for the twenty largest U.S. cities
-    cities = {
-        'New York City': (40.72, 74.00),
-        'Los Angeles': (34.05, 118.25),
-        'Chicago': (41.88, 87.63),
-        'Houston': (29.77, 95.38),
-        'Phoenix': (33.45, 112.07),
-        'Philadelphia': (39.95, 75.17),
-        'San Antonio': (29.53, 98.47),
-        'Dallas': (32.78, 96.80),
-        'San Diego': (32.78, 117.15),
-        'San Jose': (37.30, 121.87),
-        'Detroit': (42.33, 83.05),
-        'San Francisco': (37.78, 122.42),
-        'Jacksonville': (30.32, 81.70),
-        'Indianapolis': (39.78, 86.15),
-        'Austin': (30.27, 97.77),
-        'Columbus': (39.98, 82.98),
-        'Fort Worth': (32.75, 97.33),
-        'Charlotte': (35.23, 80.85),
-        'Memphis': (35.12, 89.97),
-        'Baltimore': (39.28, 76.62)
-    }
+    #init some flows
+    flow = {}
+    path = {}
+    selectpath = {}
+    flow[('10.0.0.1','10.0.0.4')] = {'ip_src':'10.0.0.1','ip_dst':'10.0.0.4','src':1,'dst':4} 
+    flow[('10.0.0.2','10.0.0.3')] = {'ip_src':'10.0.0.2','ip_dst':'10.0.0.3','src':2,'dst':3} 
+    flow[('10.0.0.4','10.0.0.2')] = {'ip_src':'10.0.0.4','ip_dst':'10.0.0.2','src':4,'dst':2} 
+    flow[('10.0.0.4','10.0.0.1')] = {'ip_src':'10.0.0.4','ip_dst':'10.0.0.1','src':4,'dst':1} 
+    flow[('10.0.0.3','10.0.0.4')] = {'ip_src':'10.0.0.3','ip_dst':'10.0.0.4','src':3,'dst':4} 
+    for flowkey in flow.keys():
+            '''
+                generator must change to list or dict
+            '''
+            path[flowkey] = []
+            for a in nx.shortest_simple_paths(g, source=flow[flowkey]['src'],
+                                             target=flow[flowkey]['dst'], weight='cost'):
+                path[flowkey].append(a)
+    for flowkey in flow:
+            selectpath[flowkey] = path[flowkey][random.randint(0, len(path[flowkey]) - 1)]
+            #print self.selectpath[flowkey]
+    tsp = recalculatebySA(selectpath , g )
+    tsp.path = path
+    tsp.flow = flow
+    tsp.copy_strategy = "deepcopy"
+    #print (tsp.flow)
+    
+    state, e = tsp.anneal()
+    print (state)
+
 
     # initial state, a randomly-ordered itinerary
-    init_state = list(cities.keys())
-    random.shuffle(init_state)
+    # init_state = list(cities.keys())
+    # random.shuffle(init_state)
 
-    # create a distance matrix
-    distance_matrix = {}
-    for ka, va in cities.items():
-        distance_matrix[ka] = {}
-        for kb, vb in cities.items():
-            if kb == ka:
-                distance_matrix[ka][kb] = 0.0
-            else:
-                distance_matrix[ka][kb] = distance(va, vb)
+    # # create a distance matrix
+    # distance_matrix = {}
+    # for ka, va in cities.items():
+    #     distance_matrix[ka] = {}
+    #     for kb, vb in cities.items():
+    #         if kb == ka:
+    #             distance_matrix[ka][kb] = 0.0
+    #         else:
+    #             distance_matrix[ka][kb] = distance(va, vb)
 
-    tsp = TravellingSalesmanProblem(init_state, distance_matrix)
-    # since our state is just a list, slice is the fastest way to copy
-    tsp.copy_strategy = "slice"  
-    state, e = tsp.anneal()
+    # initial_state = ['Phoenix','Columbus','Charlotte','New York City', 'Los Angeles', 'Houston']
+    # tsp = TravellingSalesmanProblem(init_state, distance_matrix)
+    # # since our state is just a list, slice is the fastest way to copy
+    # tsp.copy_strategy = "slice"  
+    # state, e = tsp.anneal()
 
-    while state[0] != 'New York City':
-        state = state[1:] + state[:1]  # rotate NYC to start
-    print("%i mile route:" % e)
-    for city in state:
-        print("\t", city)
+    # while state[0] != 'New York City':
+    #     state = state[1:] + state[:1]  # rotate NYC to start
+    # print("%i mile route:" % e)
+    # for city in state:
+    #     print("\t", city)
 
